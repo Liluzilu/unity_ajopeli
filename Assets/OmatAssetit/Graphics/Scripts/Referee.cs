6167732baa8247aaba7af4dabbf945635abc7751
Your Name <you@example.com>
2025-12-10 19:32:37 +0200

    valamis

--- Assets/OmatAssetit/Graphics/Scripts/Referee.cs
+++ Assets/OmatAssetit/Graphics/Scripts/Referee.cs
@@ -4,6 +4,7 @@ using TMPro;
 public class Referee : MonoBehaviour
 {
     public TMP_Text  resultText;
+    public int kierostenMaara = 3;
     private bool winnerDeclared = false;
     private void Start()
     {
@@ -12,7 +13,12 @@ public class Referee : MonoBehaviour
     private void OnTriggerEnter(Collider car)
     {
         CarIdentify id = car.GetComponent<CarIdentify>();
-        string winnerName = id.displayName;
+        if(id == null)
+        {
+            return;
+        }
+        LapCounter lap = car.GetComponent<LapCounter>();
+        //string winnerName = id.displayName;
         if (id.kind == Carkind.Player)
         {
             var validator = car.GetComponent<PelaajanKierrostarkastus>();
@@ -24,15 +30,20 @@ public class Referee : MonoBehaviour
             }
             if (!validator.AllVisitedThisLap)
             {
-                Debug.Log("Player finished the race but all checkpoints are not alright -> no victory!");
+                Debug.Log("Player finished the race but all checkpoints have not been past-> no victory!");
                 return;
             }
+            int tmpLap =lap.lapsCompleted;
+            validator.UpdateLapsText(tmpLap +1,kierostenMaara);
+            validator.ResetLap();
         }
-        if(winnerDeclared == false)
+        lap.lapsCompleted++;
+        if(winnerDeclared == false &&lap.lapsCompleted >=kierostenMaara)
         {
+            string winnerName = id.displayName;
             resultText.text = $"WINNER: {winnerName}";
             winnerDeclared = true;
-
+            GameManager.Instance.Phase = RacePhase.finished;
         }
     }
     
